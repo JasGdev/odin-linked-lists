c7aeafd190ef5e263dfda79ed2b23f71b07baefd
Jason <jason13.deng@gmail.com>
2026-02-10 12:45:19 +0900

    Setup structure for linkedlist and node

---
+++ LinkedList.js
@@ -0,0 +1,40 @@
+export class LinkedList {
+	constructor() {}
+
+	// add to end of list
+	append(value) {}
+
+	// add to start of list
+	prepend(value) {}
+
+	// total number of nodes
+	size() {}
+
+	// return first node in list (undefined if list empty)
+	head() {}
+
+	// return value of final node (undefined if list empty)
+	tail() {}
+
+	// return value of node at index (undefined if list empty)
+	at(index) {}
+
+	// remove the head node from list and return value (undefined if list empty)
+	pop() {}
+
+	// return true if the value is in list, false else
+	contains(value) {}
+
+	// return index of first node containing the value, if not found return -1
+	findIndex(value) {}
+
+	// represent linked list objects as  strings
+	// '( value ) -> ( value ) -> ( value ) -> null'
+	toString() {}
+
+	// insert new nodes with the given values at the given index (if index out of bounds throw RangeError)
+	insertAt(index, ...values) {}
+
+	// remove node at given index (if index out of bounds throw RangeError)
+	removeAt(index) {}
+}
