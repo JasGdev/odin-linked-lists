a85e714b5c7fd26c549cc07705a36c87985f34c3
Jason <jason13.deng@gmail.com>
2026-02-10 15:35:49 +0900

    Implement at(), pop(), findIndex() and tests

--- LinkedList.js
+++ LinkedList.js
@@ -5,7 +5,6 @@ export class LinkedList {
 		this.headNode = headNode;
 	}
 
-	// add to end of list
 	append(value) {
 		if (this.headNode == null) {
 			this.headNode = new Node(value);
@@ -19,13 +18,11 @@ export class LinkedList {
 		}
 	}
 
-	// add to start of list
-	prepend(value) {
+	prepend(val) {
 		let prevheadNode = this.headNode;
-		this.headNode = new Node(value, prevheadNode);
+		this.headNode = new Node(val, prevheadNode);
 	}
 
-	// total number of nodes
 	size() {
 		let size = 0;
 		if (this.headNode == null) {
@@ -40,7 +37,6 @@ export class LinkedList {
 		return size;
 	}
 
-	// return first node in list (undefined if list empty)
 	head() {
 		if (this.headNode == null) {
 			return undefined;
@@ -49,7 +45,6 @@ export class LinkedList {
 		}
 	}
 
-	// return value of final node (undefined if list empty)
 	tail() {
 		if (this.headNode == null) {
 			return undefined;
@@ -63,21 +58,89 @@ export class LinkedList {
 	}
 
 	// return value of node at index (undefined if list empty)
-	at(index) {}
+	at(index) {
+        if (this.headNode == null) {
+			return undefined;
+		} else if (index < 0 || index >= this.size()){
+            throw new Error("Out of range of linked list!")
+        } else {
+            let currentIndex = 0;
+            let currentNode = this.headNode;
+			while (currentIndex != index) {
+				currentNode = currentNode.nextNode;
+                currentIndex += 1;
+			}
+			return currentNode.value;
+        }
+
+    }
 
 	// remove the headNode node from list and return value (undefined if list empty)
-	pop() {}
+	pop() {
+        if (this.headNode == null) {
+			return undefined;
+		} else {
+            const newHead = this.headNode.nextNode
+            const returnValue = this.headNode.value
+            this.headNode = newHead
+            return returnValue
+        }
+
+    }
 
 	// return true if the value is in list, false else
-	contains(value) {}
+	contains(val) {
+        if (this.headNode == null) {
+			return false;
+		} else {
+            let currentNode = this.headNode;
+			while (currentNode.nextNode !== null) {
+                if (currentNode.value == val){
+                    return true
+                }
+				currentNode = currentNode.nextNode;
+			}
+            if (currentNode.value == val){
+                    return true
+                }
+            return false
+			
+
+        }
+
+
+    }
 
 	// return index of first node containing the value, if not found return -1
-	findIndex(value) {}
+	findIndex(val) {
+        if (this.headNode == null) {
+			return -1;
+		} else {
+            let currentIndex = 0
+            let currentNode = this.headNode;
+			while (currentNode.nextNode !== null) {
+                if (currentNode.value == val){
+                    return currentIndex
+                }
+                currentIndex += 1;
+				currentNode = currentNode.nextNode;
+			}
+            if (currentNode.value == val){
+                    return currentIndex
+                }
+            return -1
+			
+
+        }
+    }
 
 	// represent linked list objects as  strings
 	// '( value ) -> ( value ) -> ( value ) -> null'
 	toString() {
-		let currentOutput = "";
+        if (this.headNode == null) {
+			return 'null';
+		} else {
+            let currentOutput = "";
 		currentOutput = currentOutput.concat(`( ${this.headNode.value} )`);
 		let currentNode = this.headNode;
 		while (currentNode.nextNode !== null) {
@@ -86,6 +149,9 @@ export class LinkedList {
 		}
 		currentOutput = currentOutput.concat(` -> null`);
 		return currentOutput;
+        }
+
+		
 	}
 
 	// insert new nodes with the given values at the given index (if index out of bounds throw RangeError)
--- LinkedList.test.js
+++ LinkedList.test.js
@@ -42,6 +42,10 @@ describe("append() implementation", () => {
 });
 
 describe("toString() implementation", () => {
+    test("toString on empty", () => {
+		expect(list.toString()).toBe("null");
+	});
+
 	test("toString implementation test 1", () => {
 		list.append("dog");
 		list.append("cat");
@@ -93,39 +97,117 @@ describe("size() implementation", () => {
 		expect(list.size()).toBe(2);
 	});
 
-    test("size on size 3 from append and prepend LinkedList", () => {
-			list.append(1);
-			list.append(2);
-            list.prepend(3);
-			expect(list.size()).toBe(3);
-		});
+	test("size on size 3 from append and prepend LinkedList", () => {
+		list.append(1);
+		list.append(2);
+		list.prepend(3);
+		expect(list.size()).toBe(3);
+	});
 });
 
 describe("head() and tail() implementation", () => {
 	test("head and tail on empty LinkedList", () => {
 		expect(list.head()).toBe(undefined);
-        expect(list.tail()).toBe(undefined)
+		expect(list.tail()).toBe(undefined);
 	});
 
-    test("head and tail on 2x LinkedList", () => {
-        list.append(1);
+	test("head and tail on 2x LinkedList", () => {
+		list.append(1);
 		list.append(2);
-        const testNode2 = new Node(2)
-        const testNode1 = new Node(1, testNode2)
+		const testNode2 = new Node(2);
+		const testNode1 = new Node(1, testNode2);
 
 		expect(list.head()).toEqual(testNode1);
-        expect(list.tail()).toBe(2)
+		expect(list.tail()).toBe(2);
 	});
 
-    test("tail on 1x LinkedList", () => {
-        list.append(1);
-        expect(list.tail()).toBe(1)
+	test("tail on 1x LinkedList", () => {
+		list.append(1);
+		expect(list.tail()).toBe(1);
 	});
 
-    test("head on 1x LinkedList", () => {
-        list.append(1);
-        const testNode1 = new Node(1)
-        expect(list.head()).toEqual(testNode1)
+	test("head on 1x LinkedList", () => {
+		list.append(1);
+		const testNode1 = new Node(1);
+		expect(list.head()).toEqual(testNode1);
+	});
+});
+
+describe("at() implementation", () => {
+	test("at on empty LinkedList", () => {
+		expect(list.at(0)).toBe(undefined);
 	});
 
+	test("at on 2x LinkedList", () => {
+		list.append(1);
+		list.append(2);
+
+		expect(list.at(0)).toBe(1);
+		expect(list.at(1)).toBe(2);
+	});
+
+    test("at on 2x LinkedList out of range", () => {
+		list.append(1);
+		list.append(2);
+
+		expect(() => {list.at(3)}).toThrow(new Error("Out of range of linked list!"));
+        expect(() => {list.at(-1)}).toThrow(new Error("Out of range of linked list!"));
+        expect(() => {list.at(2)}).toThrow(new Error("Out of range of linked list!"));
+	});
+});
+
+describe("pop() implementation", () => {
+	test("pop() on empty LinkedList", () => {
+		expect(list.pop(0)).toBe(undefined);
+	});
+
+	test("pop() 2x on size 2 LinkedList", () => {
+		list.append(1);
+		list.append(2);
+
+        expect(list.toString()).toBe('( 1 ) -> ( 2 ) -> null');
+		expect(list.pop()).toBe(1);
+        expect(list.toString()).toBe('( 2 ) -> null');
+		expect(list.pop()).toBe(2);
+        expect(list.toString()).toBe('null');
+	});
+});
+
+describe("contains() implementation", () => {
+	test("contains() on empty LinkedList", () => {
+		expect(list.contains(0)).toBe(false);
+	});
+
+	test("contains() on size 4 LinkedList ", () => {
+		list.append(1);
+		list.append(2);
+        list.append(3);
+        list.append(4);
+
+        expect(list.contains(1)).toBe(true);
+        expect(list.contains(2)).toBe(true);
+        expect(list.contains(3)).toBe(true);
+        expect(list.contains(4)).toBe(true);
+        expect(list.contains(5)).toBe(false);
+	});
+});
+
+describe("findIndex() implementation", () => {
+	test("findIndex() on empty LinkedList", () => {
+		expect(list.findIndex(0)).toBe(-1);
+	});
+
+	test("findIndex() on size 4 LinkedList ", () => {
+		list.append(1);
+		list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(3);
+
+        expect(list.findIndex(1)).toBe(0);
+        expect(list.findIndex(2)).toBe(1);
+        expect(list.findIndex(3)).toBe(2);
+        expect(list.findIndex(4)).toBe(3);
+        expect(list.findIndex(5)).toBe(-1);
+	});
 });
